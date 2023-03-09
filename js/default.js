10ea5d7ca84ebb3fb7b85ed93f08e661934057fb
nerodevo <>
2023-03-08 21:33:17 -0600

    Initial solution

---
+++ js/default.js
@@ -0,0 +1,69 @@
+(() => {
+
+/* --- Mobile Menu --- */
+
+const button = document.getElementById('menu-toggle');
+const nav = document.getElementById('nav-header');
+
+function resizeUpdate() {
+	if (document.body.offsetWidth >= 768) {
+		nav.setAttribute('aria-expanded', false);
+		nav.classList.remove('nav-header-show');
+		button.classList.remove('nav-header-button-close');
+	}
+}
+
+function toggleMenu() {
+	if (nav.getAttribute('aria-expanded') === 'false') {
+		nav.setAttribute('aria-expanded', true);
+		nav.classList.add('nav-header-show');
+		button.classList.add('nav-header-button-close');
+		document.body.style.overflow = 'hidden';
+	} else {
+		nav.setAttribute('aria-expanded', false);
+		nav.classList.remove('nav-header-show');
+		button.classList.remove('nav-header-button-close');
+		document.body.style.overflow = 'auto';
+	}
+}
+
+window.addEventListener('resize', resizeUpdate);
+button.addEventListener('click', toggleMenu);
+
+/* --- Testimonials Slider --- */
+
+const root = document.querySelector(':root');
+
+function slideTestimonials(e) {
+	root.style.setProperty('--t-index', parseInt(e.target.value) - 1);
+}
+
+const testimonials = document.querySelectorAll('.testimonial-button');
+testimonials.forEach((testimonial) => {
+	testimonial.addEventListener('click', slideTestimonials);
+});
+
+/* --- Subscriber Form (Footer) --- */
+
+function subscribe(e) {
+	e.preventDefault();
+
+	const inputEmail = document.querySelector('#subscriber-email');
+
+	if (inputEmail.validity.valueMissing) {
+		inputEmail.classList.add('subscriber-email-error');
+		document.querySelector('#error-empty').style.display = 'block';
+	} else if (inputEmail.validity.typeMismatch) {
+		inputEmail.classList.add('subscriber-email-error');
+		document.querySelector('#error-format').style.display = 'block';
+	} else {
+		inputEmail.classList.remove('subscriber-email-error');
+		document.querySelector('#error-empty').style.display = 'none';
+		document.querySelector('#error-format').style.display = 'none';
+	}
+}
+
+document.querySelector('.subscriber-button')
+	.addEventListener('click', subscribe);
+
+})();
